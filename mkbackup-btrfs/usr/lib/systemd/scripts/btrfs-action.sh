f0a846cb1e000bba1f815003aed3dbbc20f8878c
Jakobus Schürz <jakob@xundeenergie.at>
2017-11-01 00:03:49 +0100

    xe20171101_000349

--- mkbackup-btrfs/usr/lib/systemd/scripts/btrfs-action.sh
+++ mkbackup-btrfs/usr/lib/systemd/scripts/btrfs-action.sh
@@ -3,6 +3,7 @@
 DEVICE="$1"; shift
 ACTION="$1"; shift
 MAINPID="$1"
+BTRFS=/bin/btrfs
 
 [ x"$MAINPID" = "x" ] && exit 0
-/bin/ps h -o command -p "$MAINPID" && /sbin/btrfs $ACTION cancel "$DEVICE" || exit 0
+/bin/ps h -o command -p "$MAINPID" && $BTRFS $ACTION cancel "$DEVICE" || exit 0
